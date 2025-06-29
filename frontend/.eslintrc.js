9cc141c7b7a07c9de5433438c5c9f1116344a481
AmazbAlan <almazbekovalymbek08@gmail.com>
2025-06-29 19:09:45 +0600

    updating5

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['next', 'next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
   },
-}
+};
