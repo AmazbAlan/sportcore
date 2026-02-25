31164c3186ce77308e0485a36630fce2c88c9e2f
AmazbAlan <alymbekalmazbekov0@gmail.com>
2026-02-25 19:48:42 +0600

    next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,18 @@
 // next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
+
       { protocol: 'https', hostname: 'sportcore-production.up.railway.app', pathname: '/uploads/**' },
-      { protocol: 'http',  hostname: 'localhost', port: '1337', pathname: '/uploads/**' }, // для локалки
+      { protocol: 'http', hostname: 'localhost', port: '1337', pathname: '/uploads/**' },
     ],
-    // временный обход, если нужно проверить без оптимизатора:
-    // unoptimized: true,
   },
   env: {
     NEXT_PUBLIC_STRAPI_URL: process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337',
   },
   typescript: { ignoreBuildErrors: true },
 };
+
 export default nextConfig;
